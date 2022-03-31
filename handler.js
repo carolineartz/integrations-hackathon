ca71c6b8dad11690e3bf250ae224343ec4b68f5e
Caroline Artz <ceartz@gmail.com>
2022-03-31 16:50:27 -0500

    create cocktail endpoint

--- handler.js
+++ handler.js
@@ -1,15 +1,72 @@
 "use strict";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const https = require('https')
+
+module.exports.cocktail = async (event) => {
+  try {
+    let dataString = '';
+
+    const response = await new Promise((resolve, reject) => {
+      const req = https.get('https://www.thecocktaildb.com/api/json/v1/1/random.php', (resp) => {
+        resp.on('data', (chunk) => {
+          dataString += chunk;
+        })
+
+        resp.on('end', () => {
+          const data = JSON.parse(dataString);
+          const drinkData = data.drinks[0]
+
+          const drink = {
+            id: drinkData.idDrink,
+            recipe: extractRecipe(drinkData),
+            preparation: drinkData.strInstructions,
+            img_thumbnail: drinkData.strDrinkThumb
+          }
+
+          resolve({
+            statusCode: 200,
+            body: JSON.stringify(drink, null, 2)
+          });
+        });
+      })
+
+      req.on('error', (error) => {
+        console.log(error)
+        reject({
+          statusCode: 500,
+          body: `Unexpected error ${error.message}. Please check logs.`
+        });
+      });
+    })
+
+    return response
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: `Unexpected error ${error.message}. Please check logs.`
+    }
+  }
 };
+
+
+function extractRecipe(drink) {
+  const recipe = [`*${drink.strDrink}*\n`]
+
+  const ingredients = []
+  const measurements = []
+
+  Object.entries(drink).forEach(([k, v]) => {
+    if (k.startsWith("strIngredient") && v !== null) {
+      ingredients.push(v)
+    } else if (k.startsWith("strMeasure") && v !== null) {
+      measurements.push(v)
+    }
+  })
+
+  measurements.forEach((measurement, i) => {
+    recipe.push(`${measurement} *${ingredients[i]}*`)
+  })
+
+  return recipe.join("\n")
+}
